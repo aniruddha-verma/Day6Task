dbf0fabf534b8a6183058db045bee277f013d29b
Aniruddha <anivp28@gmail.com>
2025-02-15 20:59:34 +0530

    Done!

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const searchButton = document.getElementById("searchButton");
 const climateInfo = document.getElementById("climateInfo");
 
 
-const HOT_URL = "https://images.unsplash.com/photo-1504370805625-d32c54b16100?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
-const COLD_URL = "https://images.unsplash.com/photo-1734290261674-a67538f96f12?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGNvbGQlMjB3ZWF0aGVyfGVufDB8fDB8fHww";
-const RAIN_URL = "https://images.unsplash.com/photo-1493314894560-5c412a56c17c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjIwfHxyYWlueSUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+// const HOT_URL = "https://images.unsplash.com/photo-1504370805625-d32c54b16100?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+// const COLD_URL = "https://images.unsplash.com/photo-1734290261674-a67538f96f12?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGNvbGQlMjB3ZWF0aGVyfGVufDB8fDB8fHww";
+// const RAIN_URL = "https://images.unsplash.com/photo-1493314894560-5c412a56c17c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjIwfHxyYWlueSUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
 
 
 async function fetchClimate() {
@@ -33,7 +33,6 @@ async function fetchClimate() {
         
         climateInfo.innerHTML = `
             <h3>${data.name}</h3>
-            <img src=${data.main.humidity} > 80 ? ${RAIN_URL} : ${data.main.temp} > 25 ? ${HOT_URL} : ${COLD_URL} alt=""Images">
             <p>Temperature: ${data.main.temp}°C</p>
             <p>Feels Like: ${data.main.feels_like}°C</p>
             <p>Humidity: ${data.main.humidity}%</p>
